57f8d5df6d4d5c48711aca5ee52b2bdd2fefb938
AlbertWi <160854652+AlbertWi@users.noreply.github.com>
2024-11-13 20:30:29 +0700

    Angular

--- PAWII_SI52_ANGULAR/belajar_angular/src/app/home/home.component.ts
+++ PAWII_SI52_ANGULAR/belajar_angular/src/app/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
-}
+    housingLocationList:HousingLocation[] = [
+      {
+        id    : 0,
+      name  : "Citra Grand City",
+      city  : "Palembang",
+      state : "ID",
+      photo         : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Gerbang_CitraGrand_City_Palembang.jpg/640px-Gerbang_CitraGrand_City_Palembang.jpg",
+      availableUnit :12,
+      wifi          :true,
+      laundry       :true,
+      }
+    ]
+  }
