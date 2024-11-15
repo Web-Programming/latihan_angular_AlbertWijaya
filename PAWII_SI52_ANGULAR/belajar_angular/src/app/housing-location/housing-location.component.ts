cc20c35c1a4650e8ecb3379a308ebbb9135b853a
AlbertWi <albertwijaya_2226240029@mhs.mdp.ac.id>
2024-11-15 18:03:41 +0700

    Push Angular
    Latihan_Angular

--- PAWII_SI52_ANGULAR/belajar_angular/src/app/housing-location/housing-location.component.ts
+++ PAWII_SI52_ANGULAR/belajar_angular/src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,18 @@
 import { Component,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   template: `
     <section>
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior Photo Of {{housingLocation.name}}">
       <h2 class="listing-heading">{{housingLocation.name }}</h2>
       <p class="listing-location">{{housingLocation.city }},{{housingLocation.state }}</p>
+      <a [routerLink]="['details', housingLocation.id]">Show More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css'
