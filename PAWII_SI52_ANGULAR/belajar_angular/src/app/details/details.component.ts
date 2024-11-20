6388a79f160a70601518d65e8de33600fc9fcb79
AlbertWi <albertwijaya_2226240029@mhs.mdp.ac.id>
2024-11-20 20:20:20 +0700

    angular part 3
    API

--- PAWII_SI52_ANGULAR/belajar_angular/src/app/details/details.component.ts
+++ PAWII_SI52_ANGULAR/belajar_angular/src/app/details/details.component.ts
@@ -1,29 +1,70 @@
-import { Component,inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      details works {{housingLocationId}}
-    </p>
+     <article >
+      <img class="listing-photo" [src]="housingLocation?.photo"
+        alt="Exterior photo of {{housingLocation?.name}}"/>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{housingLocation?.availableUnit}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h1 class="section-heading">Apply to live here</h1>
+        <form [formGroup]="applyForm" (submit)="submitApplyForm()">
+          <label for="first-name">First Name</label>
+          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
+          <label for="email">email</label>
+          <input type="text" id="email" formControlName="email" placeholder="Input email">
+          <button type="submit" class="primary">Apply</button>
+        </form>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-  route : ActivatedRoute = inject(ActivatedRoute);
+  route: ActivatedRoute = inject(ActivatedRoute);
   housingLocationId = 0;
-  housingService : HousingService = inject(HousingService);
-  housinglocation : HousingLocation|undefined
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined
+  applyForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('') 
+  })
 
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housinglocation = this.housingService.getHousingLocationById(this.housingLocationId);
-    console.table(this.housinglocation);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
+    console.table(this.housingLocation)
+  }
+
+  submitApplyForm(){
+    //panggil API simpan data registarsi via service
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+      
+    )
   }
 }
